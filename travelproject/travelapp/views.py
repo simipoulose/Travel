49face1a8a322aa433b7ffd930cdfde0d4d5882c
simipoulose <137863657+simipoulose@users.noreply.github.com>
2023-07-12 13:33:56 +0530

    Update views.py

--- travelproject/travelapp/views.py
+++ travelproject/travelapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Place
+from .models import People
 
 def demo(request):
     obj=Place.objects.all()
     return render(request,"index.html",{'result':obj})
+def demo1(request)
+    obj1=People.objects.all()
+    return render(request,"index.html",{'result1':obj1})
+
