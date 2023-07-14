be49580460814cbf17c23ff5f35f3f0e33490792
simipoulose <137863657+simipoulose@users.noreply.github.com>
2023-07-14 12:42:28 +0530

    Update urls.py

--- travelproject/travelapp/urls.py
+++ travelproject/travelapp/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [path('',views.demo,name='travelapp'),
-               path('',views.demo1,name='travelapp'),
+               
                # path('about/',views.about,name='about')
                ]
